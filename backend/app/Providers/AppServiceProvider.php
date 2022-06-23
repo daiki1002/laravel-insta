39db557ef1b3ff064625c2633b2f340ec6b6fb38
Kambayashi Daiki <ile.imeh.s37@gmail.com>
2022-06-23 09:51:40 +0900

    first commit

---
+++ backend/app/Providers/AppServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Pagination\Paginator;
+use App\Models\User;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Paginator::useBootstrap();
+
+        Gate::define('admin', function($user){
+            return $user->role_id == User::ADMIN_ROLE_ID
+                                ? Response::allow()
+                                : Response::deny('You must be an administrator.');
+        });
+    }
+}
